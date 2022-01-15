fa13be59e218f6754141888731a0ebefefe7b16a
seah yimin <seah_yimin@hotmail.com>
2022-01-15 20:28:19 +0800

    Added an additional word and corrected some verse references.

--- src/App.js
+++ src/App.js
@@ -5,16 +5,29 @@ import CrosswordApp from "./CrosswordApp";
 
 function App() {
   return (
-    <>
-      <header>The puzzle below is based on the whole of Ecclesiastes</header>
-      <p>Most or all of the quotes are taken from the NIV.</p>
+    <div style={{ marginLeft: "20px" }}>
+      <header style={{ fontSize: 30 }}>Ecclesiastes Crossword Puzzle</header>
       <p>
-        Please press "Reset" before starting, or else currently-filled entries
-        will not be registered.
+        <span>
+          What do people get for all the toil and anxious striving with which
+          they
+        </span>
+        <span style={{ fontWeight: "bold" }}> solve crossword puzzles </span>
+        <span>under the sun?</span>
       </p>
-      <p>Scroll down for clues.</p>
+      <div>- Clues are below.</div>
+      <div>- Most of the quotes are taken from the NIV.</div>
+      <div>
+        - Please press "Reset" before starting, or else currently-filled entries
+        will not be registered.
+      </div>
+      <div>
+        - Pro-tip: Clicking on a box you're trying to fill will highlight the
+        associated clue below. Clicking on a clue will direct you to the blank
+        word.
+      </div>
       <CrosswordApp data={data} />
-    </>
+    </div>
   );
 }
 
